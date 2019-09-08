fc1a1444317c65b11b7a901a2f5f1385bf0a2872
JOSHUA ADURAGBEMI MORAKINYO <54278775+Adurajoshua@users.noreply.github.com>
2019-09-08 15:39:32 +0100

    Updated php files

---
+++ mail.php
@@ -0,0 +1,32 @@
+<?php 
+//$name = $_POST["name'];
+//$email = $_POST['email'];
+//$subject = $_POST['subject'];
+//$message = $_POST['message'];
+//$formcontent=" From: $name \n Message: $message";
+//$recipient = "adurajoshua@gmail.com";
+//$sub = "Contact Form";
+//$mailheader = "From: $email \r\n";
+//mail($recipient, $sub,  $formcontent, $mailheader) or die("Error!");
+//echo "Thank You!";
+
+
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+  
+$headers = "from:" .$name  \n\n .$email;
+
+$mailto ="https://formsubmit.co/adurajoshua@gmail.com";
+$send_contact = mail ($mailto, $subject, $message, $headers) ;
+
+if($send_contact){
+echo "We have received your contact information";
+}
+else {
+echo "ERROR";
+}
+?>
